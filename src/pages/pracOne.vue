e7992795f8618f415ee852bbb43cb5405a9003ec
LTS <DKLTSghost@gmail.com>
2023-02-10 17:52:02 +0800

    quasar prac1 almost complete, prac2 layout complete, prac3 not yat

--- src/pages/pracOne.vue
+++ src/pages/pracOne.vue
@@ -1,30 +1,34 @@
 <template>
 
   <q-page>
+    <form @submit.prevent.stop="onSubmit" @reset.prevent.stop="onReset">
     <!-- ac pwd -->
     <div class="row">
         <div class="col q-pl-xl q-pt-xl q-pr-sm q-pb-xs">
           <!-- account -->
           <q-input
+          ref="accountRef"
           filled
+          lazy-rules
           label="帳號 *"
-          v-model="form.account.value"
-          :error="form.account.error"
-          :error-message="form.account.msg">
+          v-model="account"
+          :rules="accountRules">
             <template v-slot:append>
               <q-icon name="person"></q-icon>
             </template>
           </q-input>
+          
         </div>
         <div class="col q-pl-sm q-pt-xl q-pr-xl q-pb-xs">
           <!-- pwd -->
           <q-input
+          ref="pwdRef"
           filled
+          lazy-rules
           label="密碼 *"
+          :rules="passwordRules"
           :type="isPwd? 'password' : 'text'"
-          v-model="form.password.value"
-          :error="form.password.error"
-          :error-message="form.password.msg">
+          v-model="form.password.value">
             <template v-slot:append>
               <q-icon
               :name="isPwd? 'visibility_off' : 'visibility'"
@@ -38,45 +42,64 @@
     <div class="row">
         <div class="col q-pl-xl q-pt-xs q-pr-sm q-pb-xs">
           <!-- school -->
-          <q-input
+          <q-select
+          ref="schoolRef"
+          filled
+          lazy-rules
+          clearable 
+          v-model="form.school.value" 
+          :options="schOpt"
+          label="畢業學校 *"
+          :rules="[val => !!val || '請選擇畢業學校']"/>
+
+          <!-- <q-input
           filled
           label="畢業學校 *"
-          v-model="form.account.value"
-          :error="form.account.error"
-          :error-message="form.account.msg">
-            <!-- <template v-slot:append>
-              <q-icon name="person"></q-icon>
-            </template> -->
-          </q-input>
+          v-model="form.school.value"
+          :rules="[val => !!val || '請選擇畢業學校']"
+          :options="schOpt"
+          :error="form.school.error"
+          :error-message="form.school.msg">
+          </q-input> -->
         </div>
         <div class="col q-pl-sm q-pt-xs q-pr-xl q-pb-xs">
           <!-- graduate_date -->
           <q-input
+          ref="gradDateRef"
           filled
+          lazy-rules
           label="畢業日期 *"
-          :type="isPwd? 'password' : 'text'"
-          v-model="form.password.value"
-          :error="form.password.error"
-          :error-message="form.password.msg">
-            <!-- <template v-slot:append>
-              <q-icon
-              :name="isPwd? 'visibility_off' : 'visibility'"
-              class="cursor-pointer"
-              @click="isPwd = !isPwd"></q-icon>
-            </template> -->
+          v-model="date"
+          :rules="['date']"
+          :error="form.gradDate.error"
+          :error-message="form.gradDate.msg">
+            <template v-slot:append>
+              <q-icon name="event" class="cursor-pointer">
+                <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                  <q-date v-model="date">
+                    <div class="row items-center justify-end">
+                      <q-btn v-close-popup label="Close" color="primary" flat />
+                    </div>
+                  </q-date>
+                </q-popup-proxy>
+              </q-icon>
+            </template>
           </q-input>
         </div>
     </div>
-    <!-- old email -->
+    <!-- age email -->
     <div class="row">
         <div class="col q-pl-xl q-pt-xs q-pr-sm q-pb-xs">
-          <!-- old -->
+          <!-- age -->
           <q-input
+          ref="ageRef"
           filled
+          lazy-rules
           label="年齡 *"
-          v-model="form.account.value"
-          :error="form.account.error"
-          :error-message="form.account.msg">
+          v-model="form.age.value"
+          :rules="ageRules"
+          :error="form.age.error"
+          :error-message="form.age.msg">
             <!-- <template v-slot:append>
               <q-icon name="person"></q-icon>
             </template> -->
@@ -85,12 +108,14 @@
         <div class="col q-pl-sm q-pt-xs q-pr-xl q-pb-xs">
           <!-- email -->
           <q-input
+          ref="emailRef"
           filled
+          lazy-rules
           label="電子郵件 *"
-          :type="isPwd? 'password' : 'text'"
-          v-model="form.password.value"
-          :error="form.password.error"
-          :error-message="form.password.msg">
+          v-model="form.email.value"
+          :rules="emailRules"
+          :error="form.email.error"
+          :error-message="form.email.msg">
             <!-- <template v-slot:append>
               <q-icon
               :name="isPwd? 'visibility_off' : 'visibility'"
@@ -105,11 +130,14 @@
         <div class="col-6 q-pl-xl q-pt-xs q-pr-sm">
           <!-- mobile -->
           <q-input
+          ref="mobileRef"
           filled
+          lazy-rules
           label="手機號碼 *"
-          v-model="form.account.value"
-          :error="form.account.error"
-          :error-message="form.account.msg">
+          v-model="form.mobile.value"
+          :rules="mobileRules"
+          :error="form.mobile.error"
+          :error-message="form.mobile.msg">
             <!-- <template v-slot:append>
               <q-icon name="person"></q-icon>
             </template> -->
@@ -122,38 +150,166 @@
       </div>
     <div class="q-pa-md fit row wrap justify-center items-start content-start">
       <div>
-        <q-btn color="primary" label="提交" @click="submitLogin" />
-        <q-btn class="q-ml-sm" color="white" text-color="blue" label="重置" @click="submitLogin" />
+        <q-btn color="primary" type="submit" label="提交"/>
+        <q-btn class="q-ml-sm" type="reset" color="white" text-color="blue" label="重置"/>
       </div>
     </div>
       <!-- <div class="q-pt-lg">
         <q-btn color="primary" icon="check" label="Show another notification" @click="showNotification" />
       </div> -->
-
-
+    </form>
   </q-page>
 </template>
 
 <script setup>
+import { useQuasar } from 'quasar'
 import { ref } from 'vue';
+
+const $q = useQuasar()
 
 const toggleValue = ref(false)
 const isPwd = ref(true)
 
+const account = ref("abcdef1234567890_")
+const accountRef = ref(null)
+const pwdRef = ref(null)
+const schoolRef = ref(null)
+const gradDateRef = ref(null)
+const ageRef = ref(null)
+const emailRef = ref(null)
+const mobileRef = ref(null)
+
+const date = ref("年/月/日")
+
 const form = ref({
   account: {
-    value:"",
+    value:"abcdef1234567890_",
+    // value:"",
     account:true,
     required:true
   },
   password: {
+    value:"a12345B",
+    // value:null,
+    required:true
+  },
+  school: {
     value:"",
+    // value:null,
+    required:true
+  },
+  gradDate: {
+    value:"年/月/日",
+    required:true
+  },
+  age: {
+    value:"18",
+    // value:null,
+    required:true
+  },
+  email: {
+    value:"JohnnieCena.k.lll@gmail__132as.dff",
+    // value:null,
+    required:true
+  },
+  mobile: {
+    value:"0989534292",
+    // value:null,
     required:true
   }
 })
 
+const schOpt = ['台灣大學','清華大學','交通大學','成功大學']
+
+const accountRules = [
+  val => !!val || '請輸入帳號',
+  val => (5 < val.length && val.length < 19) || '請輸入6-18字符',
+  val => (/^[a-zA-Z][0-9_a-zA-Z]{1,17}$/.test(val)) || "只能包含數字,字母,下劃線且得字母開頭"
+]
+const passwordRules = [
+  val => !!val || '密碼不能為空',
+  val => (5 < val.length && val.length < 19) || '請輸入6-18字符',
+  val => (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9]+/.test(val)) || "請輸入英文及數字組合，須包含英文大寫跟小寫"
+  // (?=.*[a-z]) 需包含小寫
+  // (?=.*[A-Z]) 需包含大寫
+  // (?=.*\d)    需包含數字
+]
+const ageRules = [
+  val => !!val || '請輸入年齡',
+  val => (0 < val && val < 123) || '請輸入正確年齡'
+]
+const emailRules = [
+  val => !!val || '電子郵件不能為空',
+  val => (/^[^\.](?!.*\.\.)[^@]+@[a-zA-Z0-9._]+[^\.]$/.test(val)) || '電子郵件格式錯誤'
+  // dot 不能作為首尾字符, 不能連續 dot
+  // ^[^\.] 字首不能 dot
+  // (?!.*\.\.) 後面不能有 連續..
+  // [^@]+ 多個任意不含 @ 位元
+  // @[a-zA-Z0-9._]+  必須一個 @ 後面只能包含 (英文數字dot以及下劃線)
+  // [^\.]$ 字尾不能 dot
+
+  // case JohnnieCena.k.lll@gmail__132as.dff
+]
+const mobileRules = [
+  val => !!val || "手機號碼不能為空",
+  val => (/^09[0-9]{8}$/.test(val)) || "手機號碼格式錯誤, ex: 09... 必須10位數字"
+]
+
+const onSubmit = () => {
+  console.log("submit")
+  accountRef.value.validate()
+  pwdRef.value.validate()
+  schoolRef.value.validate()
+  gradDateRef.value.validate()
+  ageRef.value.validate()
+  emailRef.value.validate()
+  mobileRef.value.validate()
+
+  if (accountRef.value.hasError || pwdRef.value.hasError) {
+          // form has error
+  }
+  else if (toggleValue.value !== true) {
+    $q.notify({
+      color: 'negative',
+      message: 'You need to accept the license and terms first'
+    })
+  }
+  else {
+    $q.notify({
+      icon: 'done',
+      color: 'positive',
+      message: 'Submitted'
+    })
+  }
+}
+const onReset = () => {
+  // form.value.account = ""
+  account.value = ""
+  form.value.password = ""
+  form.value.school = ""
+  date.value = "年/月/日"
+  form.value.age = ""
+  form.value.email = ""
+  form.value.mobile = ""
+
+  accountRef.value.resetValidation()
+  pwdRef.value.resetValidation()
+  schoolRef.value.resetValidation()
+  gradDateRef.value.resetValidation()
+  ageRef.value.resetValidation()
+  emailRef.value.resetValidation()
+  mobileRef.value.resetValidation()
+}
+
+const validAccount = () => {
+  console.log(account.value.length)
+  if (account.value.length < 6) {
+    
+  }
+}
 const submitLogin = () => {
   alert("login")
 }
+
 
 </script>
--- src/pages/pracTwo.vue
+++ src/pages/pracTwo.vue
@@ -1,13 +1,325 @@
 <template>
   <q-page class="flex flex-center">
-    prac2
+    <q-table
+      title="Merchandise"
+      :rows="rows"
+      :columns="columns"
+      v-model:pagination="pagination"
+      row-key="name">
+
+      <template v-slot:top-right>
+        <q-btn class="q-mr-sm" color="primary" label="新增" @click="onClick" />
+        <!-- <q-input class="q-mr-sm" outlined bottom-slots v-model="text" label="search" :dense="dense">
+        <template v-slot:append>
+          <q-icon v-if="text !== ''" name="close" @click="text = ''" class="cursor-pointer" />
+          <q-icon name="search" />
+        </template>
+        </q-input> -->
+
+        <!-- <q-input  dense standout v-model="text" label="search"  class="q-mr-sm">
+          <template v-slot:append>
+            <q-icon v-if="text === ''" name="search" />
+            <q-icon v-else name="clear" class="cursor-pointer" @click="text = ''" />
+          </template>
+        </q-input> -->
+
+        <q-input
+              outlined
+              dense
+              debounce="300"
+              v-model="filter"
+              placeholder="Search"
+            >
+              <template v-slot:append>
+                <q-icon name="search" />
+              </template>
+            </q-input>
+
+            <!-- <q-btn
+              flat
+              round
+              dense
+              :icon="props.inFullscreen ? 'fullscreen_exit' : 'fullscreen'"
+              @click="setFs(props)"
+            > -->
+
+            <q-btn
+              flat
+              round
+              dense
+              :icon="'fullscreen'"
+              >
+
+              <q-tooltip>{{
+                props.inFullscreen ? "Exit Fullscreen" : "Toggle Fullscreen"
+              }}</q-tooltip>
+            </q-btn>
+
+            <q-btn
+              flat
+              round
+              dense
+              :icon="grid ? 'list' : 'grid_on'"
+              @click="grid = !grid"
+            >
+              <q-tooltip>{{grid ? "List" : "Grid"}}</q-tooltip>
+            </q-btn>
+
+        <q-btn
+          color="primary"
+          icon-right="archive"
+          label="Export to csv"
+          no-caps
+          @click="exportTable"
+        />
+      </template>
+
+      <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td key="item_no" :props="props">
+            {{ props.row.item_no }}
+          </q-td>
+          <q-td key="item_name" :props="props">
+              {{ props.row.item_name }}
+          </q-td>
+          <q-td key="price" :props="props">
+              {{ props.row.price }}
+          </q-td>
+          <q-td key="eff_date_from" :props="props">
+              {{ props.row.eff_date_from }}
+          </q-td>
+          <q-td key="eff_date_to" :props="props">
+              {{ props.row.eff_date_to }}
+          </q-td>
+          <q-td key="tax" :props="props">
+            <q-badge color="teal">
+              {{ props.row.tax }}
+            </q-badge>
+          </q-td>
+          <q-td key="calcium" :props="props">
+            <q-btn dense round flat color="grey" @click="editRow(props)" icon="edit"></q-btn>
+              <q-btn dense round flat color="grey" @click="deleteRow(props)" icon="delete"></q-btn>
+          </q-td>
+        </q-tr>
+      </template>
+
+    </q-table>
+
+    <!-- <q-table
+    title="Simple"
+    :rows="data"
+    :columns="columns"
+    row-key="name"
+    dense>
+    <template v-slot:body-cell-actions="props">
+      <q-td :props="props">
+        <q-btn dense round flat color="grey" @click="editRow(props)" icon="edit"></q-btn>
+        <q-btn dense round flat color="grey" @click="deleteRow(props)" icon="delete"></q-btn>
+      </q-td>          
+    </template>
+  </q-table> -->
+
   </q-page>
+
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
+import { exportFile, useQuasar } from 'quasar'
+import { ref } from 'vue'
+import json from '../assets/Practice2_Data.json'
 
-export default defineComponent({
-  name: 'IndexPage'
-})
+// console.log(json[0]['item_no'])
+//   "item_no": "01",
+//   "item_name": "BCD-535WGHSSEDS9",
+//   "price": "4200",
+//   "eff_date_from": "2023-01-05",
+//   "eff_date_to": "2024-02-05",
+//   "tax": "应税"
+
+const pagination = ref({
+      sortBy: 'desc',
+      descending: false,
+      page: 1,
+      rowsPerPage: 10,
+      rowsNumber: 8
+    })
+
+
+const columns = [
+  {
+    name: 'item_no',
+    required: true,
+    label: '商品代號',
+    align: 'left',
+    field: row => row.item_no,
+    format: val => `${val}`,
+    sortable: true
+  },
+  { name: 'item_name', align: 'center', label: '商品名稱', field: row => row.item_name, sortable: true },
+  { name: 'price', label: '價格', field: 'price', sortable: true },
+  { name: 'eff_date_from', label: '適用開始日', field: 'eff_date_from' },
+  { name: 'eff_date_to', label: '適用結束日', field: 'eff_date_to' },
+  { name: 'tax', label: '稅別', field: 'tax' },
+  { name: 'calcium', label: '操作', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+]
+
+const rows = json
+
+// const rows = [
+//   {
+//     item_no: 'Frozen Yogurt',
+//     calories: 159,
+//     fat: 6.0,
+//     carbs: 24,
+//     protein: 4.0,
+//     sodium: 87,
+//     calcium: '14%',
+//     iron: '1%'
+//   },
+//   {
+//     name: 'Ice cream sandwich',
+//     calories: 237,
+//     fat: 9.0,
+//     carbs: 37,
+//     protein: 4.3,
+//     sodium: 129,
+//     calcium: '8%',
+//     iron: '1%'
+//   },
+//   {
+//     name: 'Eclair',
+//     calories: 262,
+//     fat: 16.0,
+//     carbs: 23,
+//     protein: 6.0,
+//     sodium: 337,
+//     calcium: '6%',
+//     iron: '7%'
+//   },
+//   {
+//     name: 'Cupcake',
+//     calories: 305,
+//     fat: 3.7,
+//     carbs: 67,
+//     protein: 4.3,
+//     sodium: 413,
+//     calcium: '3%',
+//     iron: '8%'
+//   },
+//   {
+//     name: 'Gingerbread',
+//     calories: 356,
+//     fat: 16.0,
+//     carbs: 49,
+//     protein: 3.9,
+//     sodium: 327,
+//     calcium: '7%',
+//     iron: '16%'
+//   },
+//   {
+//     name: 'Jelly bean',
+//     calories: 375,
+//     fat: 0.0,
+//     carbs: 94,
+//     protein: 0.0,
+//     sodium: 50,
+//     calcium: '0%',
+//     iron: '0%'
+//   },
+//   {
+//     name: 'Lollipop',
+//     calories: 392,
+//     fat: 0.2,
+//     carbs: 98,
+//     protein: 0,
+//     sodium: 38,
+//     calcium: '0%',
+//     iron: '2%'
+//   },
+//   {
+//     name: 'Honeycomb',
+//     calories: 408,
+//     fat: 3.2,
+//     carbs: 87,
+//     protein: 6.5,
+//     sodium: 562,
+//     calcium: '0%',
+//     iron: '45%'
+//   },
+//   {
+//     name: 'Donut',
+//     calories: 452,
+//     fat: 25.0,
+//     carbs: 51,
+//     protein: 4.9,
+//     sodium: 326,
+//     calcium: '2%',
+//     iron: '22%'
+//   },
+//   {
+//     name: 'KitKat',
+//     calories: 518,
+//     fat: 26.0,
+//     carbs: 65,
+//     protein: 7,
+//     sodium: 54,
+//     calcium: '12%',
+//     iron: '6%'
+//   }
+// ]
+
+const deleteRow = (event) => {
+  event.target.parentElement.parentElement.remove();
+}
+
+function wrapCsvValue (val, formatFn, row) {
+  let formatted = formatFn !== void 0
+    ? formatFn(val, row)
+    : val
+
+  formatted = formatted === void 0 || formatted === null
+    ? ''
+    : String(formatted)
+
+  formatted = formatted.split('"').join('""')
+  /**
+   * Excel accepts \n and \r in strings, but some other CSV parsers do not
+   * Uncomment the next two lines to escape new lines
+   */
+  // .split('\n').join('\\n')
+  // .split('\r').join('\\r')
+
+  return `"${formatted}"`
+}
+
+
+const $q = useQuasar()
+
+const exportTable = () => {
+  // naive encoding to csv format
+  const content = [columns.map(col => wrapCsvValue(col.label))].concat(
+    rows.map(row => columns.map(col => wrapCsvValue(
+      typeof col.field === 'function'
+        ? col.field(row)
+        : row[ col.field === void 0 ? col.name : col.field ],
+      col.format,
+      row
+    )).join(','))
+  ).join('\r\n')
+
+  const status = exportFile(
+    'table-export.csv',
+    "\ufeff"+content,
+    'text/csv'
+  )
+
+  if (status !== true) {
+    $q.notify({
+      message: 'Browser denied file download...',
+      color: 'negative',
+      icon: 'warning'
+    })
+  }
+}
+
 </script>
